b3dc575c1b2d77229d90a3bde4d094045261e7ce
SUSHIL <aman.singh18912@gmail.com>
2024-03-08 10:51:55 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 import Navbar from './components/Navbar';
-import Form from './components/Form';
+import TextForm from './components/TextForm';
+import React,{ useState } from 'react';
+
+//import About from './components/About';
 
 function App() {
+  const[mode,setMode]=useState('light');
+
+  const toggleMode =() =>{
+        if(mode === 'light'){
+          setMode('dark');
+          document.body.style.backgroundColor ='#12385e';
+        }else{
+          setMode('light');
+          document.body.style.backgroundColor ='white';
+        }
+      }
   return (
   <>
-    <Navbar title="Converter" About="About"/>
-    <Form/>   
+    <Navbar title="TextUtils" About="About" mode={mode} toggleMode={toggleMode}/>
+    <dev className="center">
+    {/*<About/>*/}
+    <TextForm heading="Enter the text to analyze" mode={mode}/> 
+    </dev>
   </>
   );
 }
